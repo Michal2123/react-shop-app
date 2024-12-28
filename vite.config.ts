e5dc2a39defea56977f7f3b7fbb7103bfeaac657
Michał Owczarczyk <michalmarekowczarczyk@gmail.com>
2024-12-28 13:32:02 +0100

    vite config change

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-shop-app",
+  base: "./",
   test: {
     globals: true,
     environment: "jsdom",
